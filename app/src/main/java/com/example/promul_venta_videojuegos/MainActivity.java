22c63ef31f2d1d7f1a25f324a90d0bb460bd673c
carlosmilenaquesada_portatil <carlosmilenaquesada@gmail.com>
2023-10-24 17:17:46 +0200

    ultimo

--- app/src/main/java/com/example/promul_venta_videojuegos/MainActivity.java
+++ app/src/main/java/com/example/promul_venta_videojuegos/MainActivity.java
@@ -3,9 +3,29 @@ package com.example.promul_venta_videojuegos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editTextNombreUsuario;
+    EditText editTextPasswordUsuario;
+
+    Spinner spinnerGenero;
+    Spinner spinnerTitulo;
+
+    EditText editTextCantidad;
+
+    RadioGroup radioButtonGrupo;
+    RadioButton radioButtonSocio;
+    RadioButton radioButtonNoSocio;
+
+    CheckBox checkBoxCondiciones;
+    Button buttonComprar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
